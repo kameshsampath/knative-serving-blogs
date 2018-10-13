348db5c0b53c7a9dca920b0b774c625c662b4fe1
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-10-13 12:30:30 +0530

    script updates

--- part-2/java/app-container.sh
+++ part-2/java/app-container.sh
@@ -10,13 +10,13 @@ mkdir $javamnt/deployment
 
 # copy the application jar, with Knative build templates, the app sources gets loaded in the /workspace directory
 # adjust application name accordingly
-cp /workspace/${CONTEXT_DIR}/target/greeter.jar $javamnt/deployment/app.jar
+cp /workspace/${CONTEXT_DIR}/target/${JAVA_APP_NAME} $javamnt/deployment/app.jar
 
 chmod +x $javamnt/deployment/app.jar
 
 buildah config --workingdir /deployment $javacontainer
 buildah config --cmd app.jar $javacontainer
 
-buildah commit $IMAGE_NAME
+buildah commit $javacontainer $IMAGE_NAME
 
 # build push $IMAGE_NAME
