2faee4790fc630f9408fe0e4c310bfe804d02e8b
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-10-15 10:15:49 +0530

    Repo movement

--- part-1/bin/call.ps1
+++
@@ -1,7 +0,0 @@
-
-$IpAddress = kubectl get node -o 'jsonpath={.items[0].status.addresses[0].address}'
-$NodePort  = kubectl get svc knative-ingressgateway -n istio-system -o 'jsonpath={.spec.ports[?@.port==80)].nodePort}'
-$HostUrl   =  kubectl get  routes.serving.knative.dev greeter  -o jsonpath='{.status.domain}'
-
-Write-Host: "Calling Service ${HostUrl}: http://${IpAddress}:${NodePort}"
-curl -H "Host: ${HostUrl}" "http://${IpAddress}:${NodePort}"
